bd422ea3048af7b10329660b24fd0b0b26d0b83f
Xin Wang <wileywang@yunify.com>
2019-01-08 17:21:04 +0800

    add ha stacked scripts

---
+++ scripts/ha/runtime/start-client.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+K8S_HOME=$(dirname "${SCRIPTPATH}")
+
+source "${K8S_HOME}/scripts/ha/runtime/common.sh"
+
+rm /root/.ssh/authorized_keys
+ln -fs /data/authorized_keys /root/.ssh/authorized_keys
