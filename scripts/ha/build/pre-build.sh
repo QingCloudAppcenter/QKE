69e14a5e179955bf69a53dfd4996211507f27284
Xin Wang <wileywang@yunify.com>
2019-01-12 13:09:24 +0800

    add client template

--- scripts/ha/build/pre-build.sh
+++ scripts/ha/build/pre-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 gitBranch=$1
 apt-get install -y git
-git clone https://github.com/QingCloudAppcenter/kubesphere.git /opt/kubesphere
-cd /opt/kubesphere
+git clone https://github.com/QingCloudAppcenter/kubesphere.git /opt/kubernetes
+cd /opt/kubernetes
 if [ -n "$gitBranch" ]
 then
   git fetch origin $gitBranch:$gitBranch
