15d8a539f1b7e2d873db73d042be0ce0db8bca66
Xin Wang <wileywang@yunify.com>
2019-04-03 17:32:58 +0800

    remove unnecessary files

--- app/single/scripts/master/custom_metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TOKEN=`kubeadm token create --ttl 0`
-SHA=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
-
-ip=`curl http://metadata/self/host/ip`
-adminconf=$(cat "/etc/kubernetes/admin.conf"|base64 -w 0)
-JOINTOKEN="kubeadm join --token $TOKEN $ip:6443 --discovery-token-ca-cert-hash sha256:$SHA"
-echo '{"join_cmd":"'$JOINTOKEN'","adminconf":"'${adminconf}'"}'
