74399894067cf86a6a4efb9cd338bc946f55980b
Xin Wang <wileywang@yunify.com>
2019-04-10 14:56:56 +0800

    update docker 18.06.2

--- image/update-docker.sh
+++ image/update-docker.sh
@@ -14,16 +14,14 @@ apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-apt-key fingerprint 0EBFCD88
-
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
 
 apt-get update
 
-apt-get install docker-ce=18.06.1~ce~3-0~ubuntu -y
+apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
 
 docker info
 
