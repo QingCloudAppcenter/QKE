e804717d08b27a62154432af6913aece44ef89d5
Xin Wang <wileywang@yunify.com>
2019-04-17 15:41:34 +0800

    add audit and certificate rotation

---
+++ image/update-audit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "*************************"
+echo "update audit"
+echo "*************************"
+
+if [ ! -d /etc/kubernetes/audit ]
+then
+  echo "mkdir dir for audit"
+  mkdir /etc/kubernetes/audit
+fi
+
+cat << EOF > /etc/kubernetes/audit/audit-policy-file.yaml
+apiVersion: audit.k8s.io/v1
+kind: Policy
+rules:
+- level: Metadata
+EOF
+
+if [ -f /etc/kubernetes/audit/audit-policy-file.yaml ]
+then
+  echo "succeed to create audit policy file"
+else
+  echo "cannot found audit policy file"
+fi
