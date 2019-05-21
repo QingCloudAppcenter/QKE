25cba828a2a8064673a70d8c452c7740ab63605f
Xin Wang <wileywang@yunify.com>
2019-05-21 11:51:38 +0800

    add destroy ks console svc

---
+++ script/destroy-master.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+K8S_HOME=$(dirname "${SCRIPTPATH}")
+
+source "${K8S_HOME}/script/common.sh"
+source "${K8S_HOME}/script/loadbalancer-manager.sh"
+
+if [ "${HOST_ROLE}" == "master" ]
+then
+    kubectl delete -f /opt/kubernetes/k8s/kubesphere/ks-console/ks-console-svc.yaml
+fi
