8764d403b46240f19786de03afeddbd7faeedef2
Xin Wang <wileywang@yunify.com>
2019-05-24 13:49:32 +0800

    update kubesphere install package

--- image/update-kubesphere.sh
+++ image/update-kubesphere.sh
@@ -4,12 +4,12 @@ echo "*************************"
 echo "update kubesphere"
 echo "*************************"
 
-KUBESPHERE_INSTALL_PACKAGE="advanced-2.0.0.tar.gz"
+KUBESPHERE_INSTALL_PACKAGE="kubesphere-all-advanced-2.0.0-dev-20190524.tar.gz"
 
 pushd /tmp
-curl -L https://kubesphere.io/download/stable/advanced-2.0.0 > ${KUBESPHERE_INSTALL_PACKAGE}
+curl -O -k https://kubesphere-installer.pek3b.qingstor.com/nightly-build/kubesphere-all-advanced-2.0.0-dev-20190524.tar.gz
 tar -xf ${KUBESPHERE_INSTALL_PACKAGE} -C /opt
-mv /opt/kubesphere-all-advanced-2.0.0 /opt/kubesphere
+mv /opt/kubesphere-all-advanced-2.0.0-dev-20190524 /opt/kubesphere
 rm ${KUBESPHERE_INSTALL_PACKAGE}
 
 popd
