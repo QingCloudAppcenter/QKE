13b06c8969861acb95289fc3ea4ebd19c38349d9
Xin Wang <wileywang@yunify.com>
2019-05-30 15:35:10 +0800

    update ks-only installer for ELK prefix

--- image/update-kubesphere.sh
+++ image/update-kubesphere.sh
@@ -4,7 +4,7 @@ echo "*************************"
 echo "update kubesphere"
 echo "*************************"
 
-KUBESPHERE_INSTALL_PACKAGE="kubesphere-all-advanced-2.0.0-dev-20190530"
+KUBESPHERE_INSTALL_PACKAGE="kubesphere-all-advanced-2.0.0-20190530"
 
 pushd /tmp
 curl -O -k https://kubesphere-installer.pek3b.qingstor.com/nightly-build/${KUBESPHERE_INSTALL_PACKAGE}.tar.gz
