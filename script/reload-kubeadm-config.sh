ed9f4acc80524022ea67186ea5acabdc484e06d9
Xin Wang <wileywang@yunify.com>
2019-11-27 15:47:26 +0800

    update kube init

--- script/reload-kubeadm-config.sh
+++ script/reload-kubeadm-config.sh
@@ -28,14 +28,9 @@ if [ ${ENV_MASTER_COUNT} -gt 1 ]
 then
     replace_kubeadm_config_lb_ip
 fi
-# Write kubelet configuration to file "/var/lib/kubelet/config.yaml"
-# Write kubelet environment file with flags to file "/var/lib/kubelet/kubeadm-flags.env"
-kubeadm init phase kubelet-start --config  ${KUBEADM_CONFIG_PATH}
 
 if [ "${HOST_ROLE}" == "master" ]
 then
-    # Write Static Pod manifest
-    kubeadm init phase control-plane all --config ${KUBEADM_CONFIG_PATH}
     if [ "${HOST_SID}" == "1" ]
     then
         # storing the configuration used in ConfigMap "kubeadm-config" in the "kube-system" Namespace
