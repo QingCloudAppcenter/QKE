c091f02187827bd335b30963ec9c478324484e08
Lizhu You <zhuliyou@yunify.com>
2020-08-24 17:32:01 +0800

    change ks-installer log output 22 to 90

--- ansible/roles/app-role-k8s/files/opt/app/current/bin/node/k8s.sh
+++ ansible/roles/app-role-k8s/files/opt/app/current/bin/node/k8s.sh
@@ -546,7 +546,7 @@ keepKsInstallerRunningTillDone() {
 
 checkKsInstallerDone() {
   local podName; podName="$(getKsInstallerPodName)" || return $EC_KS_INSTALL_POD_ERR
-  local output; output="$(runKubectl -n kubesphere-system logs --tail 22 $podName)" || return $EC_KS_INSTALL_LOGS_ERR
+  local output; output="$(runKubectl -n kubesphere-system logs --tail 90 $podName)" || return $EC_KS_INSTALL_LOGS_ERR
   if echo "$output" | grep "^PLAY RECAP **" -A1 | egrep -o "failed=[1-9]"; then return $EC_KS_INSTALL_FAILED; fi
   echo "$output" | grep -oF 'Welcome to KubeSphere!' || return $EC_KS_INSTALL_RUNNING
   echo "$output" | grep -oF "total: $KS_MODULES_COUNT     completed:$KS_MODULES_COUNT" || return $EC_KS_INSTALL_DONE_WITH_ERR
